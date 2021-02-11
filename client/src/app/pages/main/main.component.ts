6d10d41984b02ead715f8759c80805804011ebca
Christoph Schneider <christoph.schneider@mail.ch>
2021-02-11 21:47:32 +0100

    impl basic download button functionallity

--- client/src/app/pages/main/main.component.ts
+++ client/src/app/pages/main/main.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainComponent implements OnInit {
 
+  public progressActive = false;
+  
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  public async click() {
+    if (this.progressActive) return;
+    this.progressActive = true;
+    await this.wait(1000);
+    this.progressActive = false;
+    // bytes++;
+  }
+
+  private wait(ms) {
+    return new Promise((resolve) => {
+      setTimeout(resolve, ms);
+    });
   }
 
 }
